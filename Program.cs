579970488c8dd3103aa082cb386b60a111e65974
Milacamila616 <56324531+Milacamila616@users.noreply.github.com>
2021-04-06 13:29:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula_de_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Pessoa obj = new Pessoa();
+                obj.nome = "Vayne";
+                obj.idade = 30;
+                obj.mensagem();
+        }
+    }
+
+}
